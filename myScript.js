ba6105e3d75b1d239c8ae57e5197bd424bb56030
Sunsight <76647357+nangsom2603@users.noreply.github.com>
2020-12-28 22:19:51 +0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,7 @@
+var btnChao = document.getElementById("btnChao");
+
+btnChao.addEventListener("click", function(){
+	var txtTen= document.getElementById("txtTen");
+	var ten=txtTen.value;
+	alert("xin chao ban"+ ten);
+});
